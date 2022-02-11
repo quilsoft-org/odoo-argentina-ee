03243605ba7c1d09c07fddf01aadb2a6598d4613
Jorge Obiols <jorge.obiols@gmail.com>
2022-02-10 23:42:36 -0300

    [FIX] Error al generar asiento de refundición en V15
    Se soluciono el bug primario pero estan apareciendo otros

--- account_tax_settlement/wizards/account_tax_settlement_wizard.py
+++ account_tax_settlement/wizards/account_tax_settlement_wizard.py
@@ -59,7 +59,7 @@ class AccountTaxSettlementWizard(models.TransientModel):
             res.update({'move_line_ids': active_ids})
             return res
 
-        company_ids = self._context.get('context', {}).get('company_ids')
+        company_ids = self.env.companies.ids
 
         if not company_ids or len(company_ids) != 1:
             raise ValidationError(_(
