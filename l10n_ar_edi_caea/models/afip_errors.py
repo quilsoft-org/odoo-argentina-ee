351ad5cc3f23e77e73f3953968fb41d6fcd0bb01
Jorge Obiols <jobiols@users.noreply.github.com>
2023-02-27 11:59:39 -0300

    Fix CAEA correccion indicaciones filoquin en video y otra mas que encontré (#10)
    [FIX] Estilado con Black
    
    [FIX] correcciones CAEA
    
    - def cron_caea_timeout()
      La funcion nunca ejecutó por varios errores de codigo muy basicos.
    
    - def _l10n_ar_do_afip_ws_report_invoice()
      Se agrega una excepcion para que cuando se encuentre un error y
      haga rollback no siga validando las siguientes facturas ya que
      ya se sabe que van a dar error
    
      se agrega un commit para que si una factura se logra validar
      se salve en la BD y no se vuelva a intentar validar en el caso
      de que algo falle y haga rollback.

--- l10n_ar_edi_caea/models/afip_errors.py
+++ l10n_ar_edi_caea/models/afip_errors.py
@@ -1,7 +1,4 @@
 from odoo.addons.l10n_ar_edi.models.afip_errors import WSFE_ERRORS
 from odoo import _, _lt
 
-WSFE_ERRORS.update({
-
-})
-
+WSFE_ERRORS.update({})
--- l10n_ar_edi_caea/models/res_company.py
+++ l10n_ar_edi_caea/models/res_company.py
@@ -2,29 +2,35 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     use_caea = fields.Boolean(
-        string='Allow contingency billing',
+        string="Allow contingency billing",
     )
 
     def get_active_caea(self):
         self.ensure_one()
         today = fields.Date.today()
-        return self.env['l10n_ar.afipws.caea'].search([
-            ('company_id', '=', self.id),
-            ('date_from', '<=', today),
-            ('date_to', '>=', today),
-            ('state', '=', 'active')
-        ], limit=1)
+        return self.env["l10n_ar.afipws.caea"].search(
+            [
+                ("company_id", "=", self.id),
+                ("date_from", "<=", today),
+                ("date_to", ">=", today),
+                ("state", "=", "active"),
+            ],
+            limit=1,
+        )
 
     def _l10n_ar_get_connection(self, afip_ws):
-        caea_state = self.env['ir.config_parameter'].sudo().get_param(
-            'afip.ws.caea.state', 'inactive')        
-        if caea_state == 'active' and 'ignore_active_caea' not in self.env.context:
+        caea_state = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("afip.ws.caea.state", "inactive")
+        )
+        if caea_state == "active" and "ignore_active_caea" not in self.env.context:
             return self.get_active_caea()
 
         return super()._l10n_ar_get_connection(afip_ws)
 
     def get_caea_afip_ws(self):
-        return 'wsfe'
+        return "wsfe"
